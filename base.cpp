9c6498072989c04d49885599b4adfc86281db698
tommyjohn1001 <30655730+tommyjohn1001@users.noreply.github.com>
2017-08-03 09:19:43 +0700

    Create base.cpp

---
+++ base.cpp
@@ -0,0 +1,23 @@
+#include
+#include
+#include
+
+#define USER_MAX 1000
+
+using namespace std;
+
+struct user{
+string identification_code, SSN, name, birthday, occpation, email;
+};
+
+void storeUserData(user a);
+
+int main()
+{
+user nUser[USER_MAX];
+return 0;
+}
+
+void storeUserData(user a){
+
+};
